a2fda9b8a64d35a3be208cf255cc8bd3dfcfab04
Public relations & Virtual technology <101257754+PRVTPRO@users.noreply.github.com>
2024-05-22 16:03:49 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ sudo apt install nano curl
 # Docker install
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -20,7 +20,7 @@ echo \
 sudo apt-get update
 
 # Install docker lib
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 # Test docker 
 sudo docker run hello-world
 #End  Docker install
@@ -40,7 +40,7 @@ rm -r Geekbench-6.2.1-Linux
 # End tests system
 
 # Start tests network
-sudo apt-get install curl
+sudo apt-get install curl -y
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt-get install speedtest
 sudo speedtest --accept-license
